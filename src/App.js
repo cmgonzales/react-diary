b65cd2ec8526478159d2020ebdb6caec829585b0
Chris Gonzales <cm.gonzales@yahoo.com>
2018-08-14 09:52:39 -0700

    fix submit button console.log response

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './header.jsx'
-import Input from './input.jsx'
+import User from './User.jsx'
 
 class App extends Component {
   render() {
     return (
       <div>
      <Header/>
-     <Input/>
+     <User/>
      </div>
     );
   }
---
+++ src/User.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { label, input, } from 'reactstrap';
+
+class User extends Component {
+
+    constructor(props){
+    super(props)
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    
+    this.state = {
+        value: " "
+    };
+     
+}
+    
+handleChange (e){
+        this.setState({value: e.target.value})
+}
+
+handleSubmit(e){
+    console.log(this.state.value);
+    e.preventDefault();
+}
+
+  render() {
+    return (
+      <div>
+      <form onSubmit = {this.handleSubmit}>
+      <label>Diary Entry
+      <input type="text" value={this.state.value}   onChange={this.handleChange} />
+      </label>
+      <input type = "submit" value= "Submit" />
+     </form>
+     </div>
+    );
+  }
+}
+
+export default User;
