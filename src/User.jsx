d4f847ba68d07d1bdb8494e974b2937425b87506
Chris Gonzales <cm.gonzales@yahoo.com>
2018-09-05 19:23:32 -0700

    fixed column

--- src/User.jsx
+++ src/User.jsx
@@ -29,25 +29,21 @@ handleSubmit(e){
       <div>
       <Container>
       <Row>
-      <Col xs="6" sm="4"></Col>
-      <Col md="6" sm="6">
+      <Col xs="2" sm="3"></Col>
+      <Col  md = "6" md="6">
       <label className = "title">Diary Entry
-      <br/>
-      <textarea className = "textarea" type="text" value={this.state.value}   onChange={this.handleChange} />
       </label>
+      <br/>
+      <textarea  type="text" value={this.state.value}   onChange={this.handleChange} />
       <br/>
       <Button color="primary"  value = {this.state.value} onClick = {this.handleSubmit}>submit</Button>      
       </Col>
-      <Col xs="6" sm="4"></Col>
+      <Col xs="2" sm="3"></Col>
       </Row>
       </Container>
-      <Container>
-      <Row>
-      <Col>
+      
       <List className = "list" ans={this.state.ans} /> 
-      </Col>
-      </Row>
-    </Container>
+      
     
       </div>
     );
--- src/inputs.jsx
+++ src/inputs.jsx
@@ -1,13 +1,22 @@
-
+import {Col,Row,Container} from 'reactstrap';
 import React from 'react';
 
+
 const List = props => (
+  <Container>
+  <Row>
+  <Col>
+
   <div className = "input">
+  
     {
       props.ans.map((item, index) => <p key={index}>{item}</p>)
     }
     
   </div>
+  </Col>
+  </Row>
+  </Container>
 );
 
 export default List;
